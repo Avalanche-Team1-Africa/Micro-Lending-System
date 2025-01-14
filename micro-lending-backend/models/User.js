fbfb31da81c6cdbf3c37ef4574e3333cf88c6c2c
Mark-andrew7 <kamaumark19@gmail.com>
2025-01-14 12:23:04 +0000

    Created a schema for Users, Loans and reviews

---
+++ micro-lending-backend/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    walletAddress: { type: String, required: true },
+    reputationScore: { type: Number, default: 0 },
+    dateCreated: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
