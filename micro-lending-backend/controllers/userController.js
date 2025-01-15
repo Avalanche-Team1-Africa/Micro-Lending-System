ab9016ba7551f1c9ad731065af24e0eaed3cc759
Mark-andrew7 <kamaumark19@gmail.com>
2025-01-15 06:48:04 +0000

    Added a role field for users creating an account

--- micro-lending-backend/controllers/userController.js
+++ micro-lending-backend/controllers/userController.js
@@ -4,28 +4,34 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = process.env.JWT_SECRET
 
-const signup = async(req, res) => {
-    const { username, email, password, walletAddress } = req.body;
+const signup = async (req, res) => {
+    const { username, email, password, walletAddress, role } = req.body;
 
     try {
-        const existingUser = await User.findOne({ email })
+        const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return res.status(400).json({ message: "Email already exists" })
+            return res.status(400).json({ message: "Email already exists" });
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = new User({ username, email, password: hashedPassword, walletAddress });
+
+        if (!['borrower', 'lender', 'admin'].includes(role)) {
+            return res.status(400).json({ message: "Invalid role" });
+        }
+
+        const user = new User({ username, email, password: hashedPassword, walletAddress, role });
 
         await user.save();
 
         const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
 
-        res.status(201).json({ message: "User created successfully", user, token});
-    } catch(err) {
-        res.status(400).json({ message: "Error creating user", error: err.message })
+        res.status(201).json({ message: "User created successfully", user, token });
+    } catch (err) {
+        res.status(400).json({ message: "Error creating user", error: err.message });
     }
 };
+
 
 const login = async(req, res) => {
     const { email, password } = req.body;
